f66ac3fc6759af3c7d7750b42f84c9266708380c
Lokeshkhadse <99036671+Lokeshkhadse@users.noreply.github.com>
2025-02-27 18:57:33 +0530

    Delete JunitAndMockito directory

--- JunitAndMockito/src/main/java/com/example/JunitAndMockito/Repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.JunitAndMockito.Repository;
-
-import com.example.JunitAndMockito.Entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Long> {
-
-}
