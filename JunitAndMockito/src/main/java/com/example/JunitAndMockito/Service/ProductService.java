982cfaf1f5dc299751a275cfd23fd6261b1ecb3c
Lokeshkhadse <99036671+Lokeshkhadse@users.noreply.github.com>
2025-02-27 18:46:24 +0530

    Add files via upload

---
+++ JunitAndMockito/src/main/java/com/example/JunitAndMockito/Service/ProductService.java
@@ -0,0 +1,46 @@
+package com.example.JunitAndMockito.Service;
+
+
+import com.example.JunitAndMockito.Entity.Product;
+import com.example.JunitAndMockito.Repository.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product updateProduct(Long id, Product updatedProduct) {
+        return productRepository.findById(id).map(product -> {
+            product.setName(updatedProduct.getName());
+            product.setDescription(updatedProduct.getDescription());
+            product.setPrice(updatedProduct.getPrice());
+            product.setQuantity(updatedProduct.getQuantity());
+            return productRepository.save(product);
+        }).orElseThrow(() -> new RuntimeException("Product not found"));
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
+
